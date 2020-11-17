93d1f05ff158c6031c0d103f6c55c99de5a0e6d7
ikumi-hirata <73702988+ikumi-hirata@users.noreply.github.com>
2020-11-17 09:16:31 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'tasks#index'
+
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
   get 'users/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  root to: 'tasks#index'
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
